1b73cf88955664db8d8a79798301eb381bde6f30
najongdae <najongdae@lgcns.com>
2025-09-23 14:13:23 +0900

    Deploy from av-ops-demo-front: 2025-09-23 14:13:22

---
+++ assets/trip-event-type-CLXa2VsU.js
@@ -0,0 +1,1 @@
+const e={NodeArrival:"NODE_ARRIVAL",NodeDeparture:"NODE_DEPARTURE",PassengerBoarding:"PASSENGER_BOARDING",PassengerAlighting:"PASSENGER_ALIGHTING"};export{e as T};
---
+++ assets/vehicle-status-DPFvK4uj.js
@@ -0,0 +1,1 @@
+const e={InService:"IN_SERVICE",Idle:"IDLE",Maintenance:"MAINTENANCE"};export{e as V};
